cfabf66f8f37ae0739c2de77b1362ac5073b93df
Firzamalik <151676991+firza-malik@users.noreply.github.com>
2024-06-26 14:33:23 -0700

    Add files via upload

---
+++ Pattern.java
@@ -0,0 +1,18 @@
+/*22. Write a Java program to print a pattern of stars in the shape of a pyramid*/
+public class Pattern {
+    public static void main(String[] args) {
+        int n=6;
+         for(int i=0;i<n;i++){
+            for(int j=0;j<n-i;j++){
+                System.out.print(10);
+         
+            }
+            System.out.println(" ");
+
+         }
+
+    }
+
+    
+}
+
